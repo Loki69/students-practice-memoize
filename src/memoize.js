59b89630409eac90b96358ad3565ae43e289c125
Loki69 <mur069@yandex.ru>
2015-10-01 14:27:49 +0300

    JavaScript Code Style

--- src/memoize.js
+++ src/memoize.js
@@ -1,25 +1,25 @@
 export { memoize };
 
 function memoize(func) {
-		var myCahce = {
-			key: [],
-			value: [],
-			add:function (elementId,valueElecment) {
-				this.key.push(this.calculatedNumbersKey(elementId));
-				this.value.push(valueElecment);
-			},
-			getValue: function (elementId) {
-  					if (this.key.indexOf) {
-    					return this.value[this.key.indexOf(this.calculatedNumbersKey(elementId))];
-  					}
-			},
-			calculatedNumbersKey: function (elementId) {
-  					return Array.prototype.slice.call(elementId).join(',');
-			}
-			
-		};
-	var myFunction = function(){
-			if(myCahce.getValue(arguments) === undefined){
+	var myCahce = {
+		key: [],
+		value: [],
+		add: function (elementId, valueElecment) {
+			this.key.push(this.calculatedNumbersKey(elementId));
+			this.value.push(valueElecment);
+		},
+		getValue: function (elementId) {
+	  		if (this.key.indexOf) {
+	    			return this.value[this.key.indexOf(this.calculatedNumbersKey(elementId))];
+	  		}
+		},
+		calculatedNumbersKey: function (elementId) {
+	  			return Array.prototype.slice.call(elementId).join(',');
+		}
+		
+	};
+	var myFunction = function () {
+			if (myCahce.getValue(arguments) === undefined) {
 				myCahce.add(arguments,func.apply(this,arguments));
 			}
 			return myCahce.getValue(arguments);
