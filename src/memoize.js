90c2f36c297b1222d0c584e67620b0ac400a5529
Pavel Garanin <mur069@yandex.ru>
2015-10-01 14:43:07 +0300

    JavaScript Code Style

--- src/memoize.js
+++ src/memoize.js
@@ -5,30 +5,30 @@ function memoize(func) {
 		key: [],
 		value: [],
 		add: function (elementId, valueElecment) {
-			this.key.push(this.calculatedNumbersKey(elementId));
-			this.value.push(valueElecment);
-		},
+				this.key.push(this.calculatedNumbersKey(elementId));
+				this.value.push(valueElecment);
+			},
 		getValue: function (elementId) {
-	  		if (this.key.indexOf) {
-	    			return this.value[this.getIdKey(elementId)];
-	  		}
-		},
+			  		if (this.key.indexOf) {
+			    			return this.value[this.getIdKey(elementId)];
+			  		}
+				},
 		calculatedNumbersKey: function (elementId) {
-	  			return Array.prototype.slice.call(elementId).join(',');
-		},
+	  							return Array.prototype.slice.call(elementId).join(',');
+							},
 		containsKey: function (elementId) {
-	  			return this.getValue(elementId) !== undefined;
-		},
+	  					return this.getValue(elementId) !== undefined;
+					},
 		getIdKey: function (elementId) {
-	  			return this.key.indexOf(this.calculatedNumbersKey(elementId));
-		}
+	  				return this.key.indexOf(this.calculatedNumbersKey(elementId));
+				}
 		
 	};
 	var myFunction = function () {
-			if (!myCahce.containsKey(arguments)) {
-				myCahce.add(arguments,func.apply(this,arguments));
-			}
-			return myCahce.getValue(arguments);
+						if (!myCahce.containsKey(arguments)) {
+							myCahce.add(arguments,func.apply(this,arguments));
+						}
+						return myCahce.getValue(arguments);
 	};
 	return myFunction;
 }
