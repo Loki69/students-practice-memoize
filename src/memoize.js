5c32fa67e285da86c0ce2b7e0be955a40c61bf3a
Pavel Garanin <mur069@yandex.ru>
2015-10-01 13:13:24 +0300

    adding functionality caching

--- src/memoize.js
+++ src/memoize.js
@@ -1,5 +1,31 @@
 export { memoize };
 
 function memoize(func) {
-    return func; // TODO
+		var myCahce = {
+			key: [],
+			value: [],
+			add:function (elementId,valueElecment) {
+				this.key.push(this.calculatedNumbersKey(elementId));
+				this.value.push(valueElecment);
+			},
+			getValue: function (elementId) {
+  					if (this.key.indexOf) {
+    					return this.value[this.key.indexOf(this.calculatedNumbersKey(elementId))];
+  					}
+			},
+			calculatedNumbersKey: function (elementId) {
+				console.log(Array.prototype.slice.call(elementId).join(''));
+  					return Array.prototype.slice.call(elementId).join('');
+			}
+			
+		};
+	var myFunction = function(){
+			if(myCahce.getValue(arguments) === undefined){
+				console.log('undefined');
+				console.log(arguments);
+				myCahce.add(arguments,func.apply(this,arguments));
+			}
+			return myCahce.getValue(arguments);
+	};
+	return myFunction;
 }
