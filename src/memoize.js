a943b5054dbd20b45663d9bb63100fc060c27b3b
Pavel Garanin <mur069@yandex.ru>
2015-10-01 14:38:34 +0300

    refactoring

--- src/memoize.js
+++ src/memoize.js
@@ -10,16 +10,22 @@ function memoize(func) {
 		},
 		getValue: function (elementId) {
 	  		if (this.key.indexOf) {
-	    			return this.value[this.key.indexOf(this.calculatedNumbersKey(elementId))];
+	    			return this.value[this.getIdKey(elementId)];
 	  		}
 		},
 		calculatedNumbersKey: function (elementId) {
 	  			return Array.prototype.slice.call(elementId).join(',');
+		},
+		containsKey: function (elementId) {
+	  			return this.getValue(elementId) !== undefined;
+		},
+		getIdKey: function (elementId) {
+	  			return this.key.indexOf(this.calculatedNumbersKey(elementId));
 		}
 		
 	};
 	var myFunction = function () {
-			if (myCahce.getValue(arguments) === undefined) {
+			if (!myCahce.containsKey(arguments)) {
 				myCahce.add(arguments,func.apply(this,arguments));
 			}
 			return myCahce.getValue(arguments);
