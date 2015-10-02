174f30664cf292558f99b0686ceb2e5c3d92cc11
Pavel Garanin <mur069@yandex.ru>
2015-10-02 17:25:50 +0300

    change of logic functions

--- src/memoize.js
+++ src/memoize.js
@@ -1,34 +1,75 @@
-export { memoize };
+export {
+	memoize
+};
+
 
 function memoize(func) {
 	var myCahce = {
 		key: [],
-		value: [],
-		add: function (elementId, valueElecment) {
-			this.key.push(this.calculatedNumbersKey(elementId));
-			this.value.push(valueElecment);
+		values: [],
+		add: function(elementId, valueElecment) {
+			this.key.push(this.calKey(elementId));
+			this.values.push(valueElecment);
 		},
-		getValue: function (elementId) {
-	  		if (this.key.indexOf) {
-	    			return this.value[this.getIdKey(elementId)];
-	  		}
-		},
-		calculatedNumbersKey: function (elementId) {
-	  			return Array.prototype.slice.call(elementId).join(',');
-		},
-		containsKey: function (elementId) {
-	  			return this.getValue(elementId) !== undefined;
-		},
-		getIdKey: function (elementId) {
-	  			return this.key.indexOf(this.calculatedNumbersKey(elementId));
-		}
-		
-	};
-	var myFunction = function () {
-			if (!myCahce.containsKey(arguments)) {
-				myCahce.add(arguments,func.apply(this,arguments));
+		getValue: function(elementId) {
+			if (this.key.indexOf) {
+				return this.values[this.getIdKey(elementId)];
 			}
-			return myCahce.getValue(arguments);
+		},
+		calKey: function(elementId) {
+			var myvar = "";
+			if (elementId.length === 0) {
+				myvar += "empty arguments";
+			} else {
+				for (var i = 0; i < elementId.length; i++) {
+					myvar += this.generateCahe(elementId[i]);
+				}
+			}
+			return myvar;
+		},
+		generateCahe: function(element) {
+			switch (typeof(element)) {
+				case "object":
+					return this.calculatedForObjectTypes(element);
+				case "undefined":
+					return "undefined:{}";
+				default:
+					return this.calculatedForScalarTypes(element);
+			}
+		},
+		calculatedForObjectTypes: function(typeObject) {
+			var cahe = "";
+			if (typeObject) {
+				for (var arg in typeObject) {
+					cahe += arg;
+					cahe += ":";
+					cahe += this.generateCahe(typeObject[arg]);
+				}
+			} else {
+				cahe += "null:{}";
+			}
+			return cahe;
+		},
+		calculatedForScalarTypes: function(typeScalar) {
+			var cahe = "";
+			cahe += typeof(typeScalar);
+			cahe += ":{";
+			cahe += typeScalar;
+			cahe += "}";
+			return cahe;
+		},
+		containsKey: function(elementId) {
+			return this.getIdKey(elementId) === -1;
+		},
+		getIdKey: function(elementId) {
+			return this.key.indexOf(this.calKey(elementId));
+		},
+	};
+	var myFunction = function() {
+		if (myCahce.containsKey(arguments)) {
+			myCahce.add(arguments, func.apply(this, arguments));
+		}
+		return myCahce.getValue(arguments);
 	};
 	return myFunction;
 }
--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -12,34 +12,39 @@ describe("Memoized function", function () {
         return Math.round(Math.sqrt(power));
     }
 
-    function concatArray() {
-        let xs = [...arguments];
-        let result = xs.map(x => typeof x).reduce((sum, x) => sum + x);
-        return result;
+    function grep(pattern, ...items) {
+        if (!(pattern instanceof RegExp)) {  // Special case
+            return undefined;
+        }
+        return items.map(String).filter(pattern.test.bind(pattern));
     }
 
-    var absSpy;
-    var memoizedAbs;
-    var concatArraySpy;
-    var memoizedConcatArray;
+    var absSpy, grepSpy;
+    var memoizedAbs, memoizedGrep;
 
     beforeEach(function () {
         absSpy = chai.spy(abs);
-        concatArraySpy = chai.spy(concatArray);
-
         memoizedAbs = memoize(absSpy);
-        memoizedConcatArray = memoize(concatArraySpy);
+        grepSpy = chai.spy(grep);
+        memoizedGrep = memoize(grepSpy);
     });
 
     it("should delegate calls to target function", function() {
         expect(memoizedAbs(0, 1)).to.equal(1);
         expect(memoizedAbs(3, 4)).to.equal(5);
+        expect(memoizedGrep(/\d+/, "abc", "123", "def")).to.deep.equal(["123"]);
     });
 
     it("should return correct values in case of the consequent calls with identical arguments", function() {
         for (var i = 0; i < 2; i++) {
             expect(memoizedAbs(3, 4)).to.equal(5);
         }
+    });
+
+    it("should cache undefined result as legal (non empty) value", function() {
+        expect(memoizedGrep()).to.equal(undefined);
+        expect(memoizedGrep()).to.equal(undefined);
+        expect(grepSpy).to.have.been.called.once();
     });
 
     it("should cache results of the consequent calls with identical arguments", function() {
@@ -58,41 +63,14 @@ describe("Memoized function", function () {
     });
 
     it("should use all arguments to compute cache key", function() {
-        for (var i = 0; i < 2; i++) {
-            memoizedAbs(1, 2, 3, 4, 5);
-        }
-        expect(absSpy).to.have.been.called.once();
+        memoizedAbs(1, 2, 3, 4, 5);
+        memoizedAbs(1, 2, 3, 4);
+        expect(absSpy).to.have.been.called.twice();
     });
 
-    it("should executed without errors", function() {
-        expect(memoizedAbs(1, 2, null));
-        expect(memoizedAbs(1, 2, undefined));
-        expect(memoizedAbs(1, 2, NaN));
-        expect(concatArray(1, [1,2,3,4,'asdfasdf']));
-    });
-
-    it("should return different values", function() {
-        expect(memoizedAbs(1, 2)).to.equal(2);
-        expect(memoizedAbs(12)).to.equal(12);
-
-        expect(memoizedConcatArray(1, 2, NaN)).to.equal('numbernumbernumber');
-        expect(memoizedConcatArray(1, 2, undefined)).to.equal('numbernumberundefined');
-        expect(memoizedConcatArray(1, 2, null)).to.equal('numbernumberobject');
-
-        expect(memoizedConcatArray(112,['',3,4,'asdfasdf'])).to.equal('numberobject');
-        expect(memoizedConcatArray(11,[2,3,4,'asdfasdf'],null)).to.equal('numberobjectobject');
-
-
-    });
-
-    it("should understand the difference between Object", function() {
-        const ARGS0 = [112, {name: 'Ван Гог'}],
-              ARGS1 = [112, {status: 'DEAD'}];
-
-        memoizedConcatArray(...ARGS0);
-        expect(concatArraySpy).to.have.been.called();
-
-        memoizedConcatArray(...ARGS1);
-        expect(concatArraySpy).to.have.been.called.twice();
+    it("should use complicated enough key generator to distinguish different types", function() {
+        expect(memoizedGrep(/\d+/, "1", 2, "true")).to.deep.equal(["1", "2"]);
+        expect(memoizedGrep(/\d+/, 1, "2", true)).to.deep.equal(["1", "2"]);
+        expect(grepSpy).to.have.been.called.twice();
     });
 });
